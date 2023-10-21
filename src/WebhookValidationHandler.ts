fb25738955b23ea2168d6a4d27de875aa5b71a71
ssierrac <sebastian.sierrajc@gmail.com>
2023-10-21 13:23:43 -0500

    test: diff message for protocols send json

--- src/WebhookValidationHandler.ts
+++ src/WebhookValidationHandler.ts
@@ -87,8 +87,9 @@ class ValidateWebhookFuntion implements LambdaInterface {
     private async publishPayload(payload: any) {
         try {
             const message = {
-                default: `Push event received for ${payload.repository.full_name} repo`,
-                lambda: payload,
+                email: `Push event received for ${payload.repository.full_name} repo`,
+                'email-json': `Push event received for ${payload.repository.full_name} repo`,
+                default: payload,
             };
 
             const command = new PublishCommand({
